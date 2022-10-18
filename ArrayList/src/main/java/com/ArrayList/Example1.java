a4c2ea47ab4d187f684d1725cee634542050bd70
PrithwishShaw <prithwishshaw23@gmail.com>
2022-10-18 17:48:05 +0530

    Generic

---
+++ ArrayList/src/main/java/com/ArrayList/Example1.java
@@ -0,0 +1,16 @@
+package com.ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> fruits = new ArrayList<String>();
+		
+		
+	}
+
+}
